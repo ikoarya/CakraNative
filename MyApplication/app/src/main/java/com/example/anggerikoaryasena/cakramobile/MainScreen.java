e7881c2c65fae586da3d879d688ad5ad789c3299
Anggeriko Aryasena <riy_cow@yahoo.com>
2015-11-10 19:44:22 +0700

    New belum diotak atik

---
+++ MyApplication/app/src/main/java/com/example/anggerikoaryasena/cakramobile/MainScreen.java
@@ -0,0 +1,13 @@
+package com.example.anggerikoaryasena.cakramobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+}
