d4a2908391b01493435a4ae0344f8be2ab94830a
Anggeriko Aryasena <riy_cow@yahoo.com>
2015-11-16 15:28:21 +0700

    Splash + Halaman Awal (belum 100%)

--- MyApplication/app/src/main/java/com/example/anggerikoaryasena/cakramobile/MainScreen.java
+++ MyApplication/app/src/main/java/com/example/anggerikoaryasena/cakramobile/MainScreen.java
@@ -1,13 +1,44 @@
 package com.example.anggerikoaryasena.cakramobile;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        Typeface tipe = Typeface.createFromAsset(getAssets(), "fonts/comicbookfun.ttf" );
+
+        Button entrybutton = (Button) findViewById(R.id.masukbut);
+        entrybutton.setText("Masuk");
+        entrybutton.setTextSize(24);
+        entrybutton.setTypeface(tipe);
+        entrybutton.setTextColor(Color.parseColor("#FFFFFF"));
+
+        Button daftarbutton = (Button) findViewById(R.id.daftarbut);
+        daftarbutton.setText("Daftar");
+        daftarbutton.setTextSize(24);
+        daftarbutton.setTypeface(tipe);
+        daftarbutton.setTextColor(Color.parseColor("#FFFFFF"));
+
+        entrybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v, MotionEvent event) {
+                
+            }
+
+        });
     }
 }
---
+++ MyApplication/app/src/main/java/com/example/anggerikoaryasena/cakramobile/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.anggerikoaryasena.cakramobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by Anggeriko Aryasena on 11/13/2015.
+ */
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Thread timerthread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                    Log.d("cakracoba", "masuk try");
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(SplashScreen.this, MainScreen.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerthread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
