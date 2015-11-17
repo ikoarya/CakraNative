69c5aaeaa4e359c2a229d71d9d27ba97128727cd
Anggeriko Aryasena <riy_cow@yahoo.com>
2015-11-18 00:51:10 +0700

    splash+main+login+daftar

--- MyApplication/app/src/main/java/com/example/anggerikoaryasena/cakramobile/MainScreen.java
+++ MyApplication/app/src/main/java/com/example/anggerikoaryasena/cakramobile/MainScreen.java
@@ -35,4 +35,6 @@ public class MainScreen extends AppCompatActivity {
 
 
     }
+
+
 }
---
+++ MyApplication/app/src/main/java/com/example/anggerikoaryasena/cakramobile/daftar.java
@@ -0,0 +1,78 @@
+package com.example.anggerikoaryasena.cakramobile;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+/**
+ * Created by Anggeriko Aryasena on 11/17/2015.
+ */
+public class daftar extends AppCompatActivity {
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daftar);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        Typeface tipe = Typeface.createFromAsset(getAssets(), "fonts/comicbookfun.ttf" );
+
+        TextView judul = (TextView) findViewById(R.id.judul);
+        judul.setText("TULISKAN IDENTITAS");
+        judul.setTextSize(26);
+        judul.setTypeface(tipe);
+        judul.setTextColor(Color.parseColor("#FFFFFF"));
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DATE);
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            //showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
+}
